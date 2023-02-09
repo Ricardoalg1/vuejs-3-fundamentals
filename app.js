d33e7d90b5c4b9e13a18af309a499c860ab11e94
ricardo algarin <rijaal1989g@gmail.com>
2023-02-09 15:29:59 -0500

    register component locally title inside of title picker

--- app.js
+++ app.js
@@ -1,63 +1,90 @@
+let titleComponentObj = {
+     template: '#title-template',
+
+            props: {
+        
+                id: {type:Number, required:true, default:00},
+                name: { type: String, default: 'Sprites' },
+                age: { type: Number, default: 30 },
+                gender: { type: String, default: 'Male' },
+                hash: { type: String, default: '1' },
+                scr:''
+            
+            }
+}
+
+
 const app = Vue.createApp({
 
-    data() {
-        return {
-
-            familyMembers: [
-                {
-                 
-                    name: 'Ricardo',
-                    age: 32,
-                    gender: 'Male'
-                   
-                },
-                {
-                    
-                    name: 'Alison',
-                    age: 30,
-                     gender: 'Female'
-                }, 
-                {
-                    
-                    name: 'Luz',
-                    age: 63,
-                    gender: 'Female'
-                },
-                {
-                  
-                    name: 'Jerry',
-                    age: 2,
-                   gender: 'Male'
-                }
-             
-            ]
-        }
-    }
 
 })
-    .component('click-counter', {
-        template: '#click-counter-template',
-        data() {
+    .component('title-picker', {
+        
+        template: '#title-picker-template',
+        components: { titleComponent : titleComponentObj},
+            data() {
             return {
-                Count: 0
-            }
-        },
-        methods: {
-            increment() {
-                this.Count++
+
+                familyMembers: [
+                    {
+                        id:1,
+                        name: 'Ricardo',
+                        age: 32,
+                        gender: 'Male',
+                        hash: '205e460b479e2e5b48aec07710c08d50'
+                    
+                    },
+                    {
+                        id:2,
+                        name: 'Alison',
+                        age: 30,
+                        gender: 'Female',
+                        hash: '0'
+                    }, 
+                    {
+                        id:3,
+                        name: 'Luz',
+                        age: 63,
+                        gender: 'Female',
+                        hash: '205e460b479e2e5b48aec07710c08d50'
+                    },
+                    {
+                    id:4,
+                        name: 'Jerry',
+                        age: 2,
+                        gender: 'Male',
+                        hash: '0'
+                    } ,
+                    {
+                    id:5,
+                        name: 'Toby',
+                        age: 13,
+                        gender: 'Male',
+                        hash: '1234567890'
+                    }
+                
+                ]
             }
         }
         
-    })
-    .component('title-component', {
-        template: '#title-template',
-
-        props: {
-       
-            name: { type: String, default: 'Sprites' },
-            age: { type: Number, default: 30 },
-            gender: { type: String, default: 'Male' }
-        }
-    })
+        })
     
+        
+
+        .component('click-counter', {
+            template: '#click-counter-template',
+           
+            data() {
+                return {
+                    Count: 0
+                }
+            },
+            methods: {
+                increment() {
+                    this.Count++
+                }
+            }
+            
+        })
+        
 .mount('#app')
